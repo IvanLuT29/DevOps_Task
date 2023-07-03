6597dc8d3f9b03dbae47bb67ba6f21f3ad29e879
Ivan <iluzjuk@gmail.com>
2023-07-03 13:59:10 +0300

    V4

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,42 +1,17 @@
 provider "aws" {
   access_key = "AWS_ACCESS_KEY_ID"
   secret_key = "AWS_SECRET_ACCESS_KEY"
-  region = "eu-central-1"  
+  region     = "eu-central-1"
 }
 
 resource "aws_instance" "web_instance" {
   ami           = "ami-0b2ac948e23c57071"  # Укажите желаемый AMI ID
-  instance_type = "t2.micro"      # Укажите желаемый тип инстанса
+  instance_type = "t2.micro"                # Укажите желаемый тип инстанса
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
   tags = {
     Name = "WebServer"
   }
 
-}
-resource "aws_security_group" "my_webserver" {
-  name        = "WebServer Security Group"
-  description = "WebServer"
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  
-  }
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-}
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
@@ -45,3 +20,30 @@ resource "aws_security_group" "my_webserver" {
       "sudo systemctl enable httpd"
     ]
   }
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "WebServer"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
